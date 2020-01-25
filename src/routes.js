3f874620bd5ceab74e6fafec4e61b50ded00f140
jmamadeu <jmamadeu2000@gmail.com>
2020-01-25 13:08:06 +0100

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Layout from "./hoc/Layout";
+
+export default function Routes() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Layout>
+  );
+}
